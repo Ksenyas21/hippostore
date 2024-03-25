a36e3f066e05e3e04ff50455ac0f3263778a75e2
dlkksny <kseniadolgan15@gmail.com>
2024-03-25 17:57:15 +0200

    add image slider

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -6,6 +6,7 @@ import {Skeleton} from "@/components/ui/skeleton";
 import Link from "next/link";
 import {cn, formatPrice} from "@/lib/utils";
 import {PRODUCT_CATEGORIES} from "@/config";
+import ImageSlider from "@/components/ImageSlider";
 
 interface ProductListingProps {
     product: Product | null;
@@ -24,17 +25,24 @@ const ProductListing = ({ product, index }:ProductListingProps) => {
 
     const label = PRODUCT_CATEGORIES.find(({value}) => value === product?.category)?.label
 
+    const validUrls = product?.images
+        .map(({image}) =>
+        (typeof image === 'string' ? image : image.url))
+        .filter(Boolean) as string[]
+
     if (!product || !ifVisible) return <ProductPlaceholder />
     if(ifVisible && product) {
-        return <Link
+        return (
+            <Link
             className={cn(
                 "invisible h-full w-full cursor-pointer group/main",
                 {
                     "visible animate-in fade-in-5": ifVisible
                 }
                 )}
-            href={`/products/${product.id}`}>
+            href={`/product/${product.id}`}>
                 <div className="flex flex-col w-full">
+                    <ImageSlider urls={validUrls} />
                     <h3 className="mt-4 font-medium text-sm text-gray-700">
                         {product.name}
                     </h3>
@@ -46,6 +54,7 @@ const ProductListing = ({ product, index }:ProductListingProps) => {
                     </p>
                 </div>
         </Link>
+        )
     }
 }
 
